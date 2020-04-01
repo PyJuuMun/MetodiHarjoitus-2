62dab88db21d942d498ede2798f714e9b177b833
PyJuuMun <pyjuumun@hotmail.com>
2020-04-01 12:38:37 +0300

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetodiHarjoitus_2
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public Person Friend;
+        public List<string> Hobbies = new List<string>();
+
+
+        public Person()
+        {
+
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("Terve, miten menee");
+        }
+
+        public void FormanGreet()
+        {
+            Console.WriteLine($"Hei, olen {Name} ja olen {Age} vuotta vanha.");
+        }
+
+        public bool AgeIfElse18()
+        {
+            if (Age >= 18)
+            {
+                FormanGreet();
+                Console.WriteLine("Olen täysi-ikäinen.");
+                return true;
+            }
+
+            else
+            {
+                FormanGreet();
+                Console.WriteLine("Olen alaikäinen.");
+                return false;
+            }
+        }
+
+        public void GrowingOld(int years)
+        {
+            Age += years;
+        }
+
+        public string Comparison()
+        {
+            if (Friend == null)
+            {
+                
+                return $"{Name}, olet yksinäinen.";
+            }
+
+            else if (Age > Friend.Age)
+            {
+                return $"{Name}, olet vanhempi kuin {Friend.Name}.";
+            }
+
+            else
+            {
+                return $"{Friend.Name} on vanhempi kuin sinä, {Name}.";
+            }
+            
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MetodiHarjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person Class objectit luotu, sisältää nimet ja iät
+            Person person1 = new Person();
+            person1.Name = "Mari";
+            person1.Age = 16;
+            person1.Hobbies.Add("jalkapallo");
+            person1.Hobbies.Add("tennis");
+            person1.Hobbies.Add("uinti");
+
+            Person person2 = new Person();
+            person2.Name = "Tommi";
+            person2.Age = 22;
+            person2.Hobbies.Add("miniatyyri-mallit");
+            
+            Person person3 = new Person();
+            person3.Name = "Anna";
+            person3.Age = 28;
+            person3.Hobbies.Add("miniatyyri-mallit");
+            person3.Hobbies.Add("drone-lentäminen");
+
+            //'personX.Friend = X;' kirjoitettu tähän niin että objecti 3 on luoto ennen kuin niitä yhdistetään edellä olevalla koodilla
+            person2.Friend = person3;
+            person3.Friend = person2;
+
+            person3.Greet();
+            //person1.FormanGreet();
+            person3.AgeIfElse18();
+
+            Console.WriteLine(person1.Comparison());
+        }
+    }
+}
